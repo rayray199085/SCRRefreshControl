bf8756d392e57274e56e6941513cd5e82caf44ae
Rui Cao <rayray199085@gmail.com>
2019-05-03 21:05:54 +1000

    For A Test

---
+++ SCRRefreshControl/SCRRefreshControl/MyFrame/SCRRefreshControl.swift
@@ -0,0 +1,19 @@
+//
+//  SCRRefreshControl.swift
+//  SCRRefreshControl
+//
+//  Created by Stephen Cao on 3/5/19.
+//  Copyright © 2019 Stephen Cao. All rights reserved.
+//
+
+import UIKit
+
+class SCRRefreshControl: NSObject {
+    
+    override init() {
+        super.init()
+    }
+    func sayHello(){
+        print("Hello World!")
+    }
+}
---
+++ SCRRefreshControl/SCRRefreshControl/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  SCRRefreshControl
+//
+//  Created by Stephen Cao on 3/5/19.
+//  Copyright © 2019 Stephen Cao. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        let rc = SCRRefreshControl()
+        rc.sayHello()
+    }
+
+
+}
+
